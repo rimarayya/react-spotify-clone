93c91a666e32388ed427692871b15124495a7e9b
rima <rima@MD-01.foodco.local>
2025-08-10 06:43:46 +0300

    (feat): add Discover page with initial layout and logic

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,52 @@
-const Discover = () => <div>Discover</div>;
+import { genres } from "../assets/constants";
+import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/Deezer";
+import { useDispatch, useSelector } from "react-redux";
 
-export default Discover;
+export default function Discover() {
+  const dispatch = useDispatch();
+
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const genreTitle = "Pop";
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.tracks?.data.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+            activeSong={activeSong}
+            isPlaying={isPlaying}
+            data={data}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
